651b511f1a43c3169cba55f3a79736e4fc7556aa
amoyag00 <amoyag00@estudiantes.unileon.es>
2018-01-17 18:50:44 +0100

    ElementProxy

--- blucyan/src/Logic/ElementProxy.java
+++ blucyan/src/Logic/ElementProxy.java
@@ -1,11 +1,47 @@
 package Logic;
 
+import javax.swing.ImageIcon;
+
 public class ElementProxy {
+        private ImageIcon cover;
+        private String name;
+        private String id;
+        private Element realElement;
 
+    public ImageIcon getCover() {
+        return cover;
+    }
+
+    public void setCover(ImageIcon cover) {
+        this.cover = cover;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+  
+    
 	public Element getRealInstance() {
-		// TODO - implement ElementProxy.getRealInstance
-		throw new UnsupportedOperationException();
-	}
+            if (this.realElement== null){
+                realElement= Facade.getInstance().get(id,Element.class){
+                
+            }
+                return this.realElement;
+        }
+}
 
-	private Element realElement;
+	
 }
