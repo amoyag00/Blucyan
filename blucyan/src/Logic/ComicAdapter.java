d3c5e4d9e8350c8cfb405627e73b7aa509c5761a
slebra00 <slebra00@estudiantes.unileon.es>
2018-01-18 17:19:20 +0100

    Adapters first implementation

--- blucyan/src/Logic/ComicAdapter.java
+++ blucyan/src/Logic/ComicAdapter.java
@@ -1,4 +1,42 @@
 package Logic;
 
-public class ComicAdapter implements IAdapter extends ComicEntry  {
+import static java.lang.Integer.parseInt;
+
+public class ComicAdapter extends ComicEntry implements IAdapter   {
+    
+    @Override
+    public Entry createEntry(String[] params){
+
+        ComicEntry comicEntry = new ComicEntry();
+        
+        comicEntry.setComicID(params[0]);
+        comicEntry.setValoration(parseInt(params[1]));
+        comicEntry.setNumReadChapters(parseInt(params[2]));
+        //comicEntry.setStatus(params[3]);
+
+        return comicEntry;
+    }
+    
+    @Override
+    public Entry modEntry(Entry entry, String attribute, String newValue){
+
+        ComicEntry comicEntry = (ComicEntry) entry;
+        
+        switch(attribute){
+        
+            case "status": comicEntry.setStatus(newValue);
+                            break;
+            case "numReadChapters": comicEntry.setNumReadChapters(parseInt(newValue));
+                            break;
+            case "valoration": comicEntry.setValoration(parseInt(newValue));
+                            break;
+
+        }
+       
+        return comicEntry;
+    }
+
+    
+    
+
 }
--- blucyan/src/Logic/ShowAdapter.java
+++ blucyan/src/Logic/ShowAdapter.java
@@ -1,4 +1,40 @@
 package Logic;
 
+import static java.lang.Integer.parseInt;
+
 public class ShowAdapter extends ShowEntry implements IAdapter {
+    
+      @Override
+    public Entry createEntry(String[] params){
+
+        ShowEntry showEntry = new ShowEntry();
+        
+        showEntry.setShowID(params[0]);
+        showEntry.setValoration(parseInt(params[1]));
+        showEntry.setNumWatchedEpisodes(parseInt(params[2]));
+        showEntry.setStatus(params[3]);
+
+        return showEntry;
+    }
+    
+    @Override
+    public Entry modEntry(Entry entry, String attribute, String newValue){
+
+        ShowEntry showEntry =  (ShowEntry) entry;
+        
+        switch(attribute){
+        
+            case "status": showEntry.setStatus(newValue);
+                            break;
+            case "numWatchedEpisodes": showEntry.setNumWatchedEpisodes(parseInt(newValue));
+                            break;
+            case "valoration": showEntry.setValoration(parseInt(newValue));
+                            break;
+
+        }
+
+        
+        return showEntry;
+    }
+    
 }
